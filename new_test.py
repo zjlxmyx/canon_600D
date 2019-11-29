ac7193377a40d12bb1a147c04fd6144b0daf5cfb
zjlxmyx <maoyanxin1991@hotmail.com>
2019-11-29 16:02:03 +0100

    first version

--- new_test.py
+++ new_test.py
@@ -2,11 +2,11 @@ import CanonLib
 
 c = CanonLib.CanonCamera()
 err = c.Init_Camera()
-if err is not None:
+if type(err) is str:
     print(err)
 err = c.set_LiveView_ready()
-if err is not None:
+if type(err) is str:
     print(err)
 err = c.get_Live_image()
-if err is not None:
+if type(err) is str:
     print(err)
