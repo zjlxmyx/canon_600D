0b4b926d1adf0f621c33b8a1c719957b44aaf029
zjlxmyx <maoyanxin1991@hotmail.com>
2019-12-02 12:07:06 +0100

    first version

--- canon_test.py
+++ canon_test.py
@@ -4,8 +4,9 @@ import CanonLib
 from ui_canon_test import Ui_MainWindow
 from PyQt5 import QtCore, QtGui, QtWidgets
 from turbojpeg import TurboJPEG
+import time
 
-jpeg = TurboJPEG("C:\\turbojpeg.dll")
+jpeg = TurboJPEG("turbojpeg.dll")
 
 
 class GUIMainWindow(Ui_MainWindow, QtWidgets.QMainWindow):
@@ -20,6 +21,7 @@ class GUIMainWindow(Ui_MainWindow, QtWidgets.QMainWindow):
 
     def init_UI(self):
         self.pushButton_camera.clicked.connect(self.camera_task)
+        self.pushButton_capture.clicked.connect(self.camera_capture)
 
     def camera_task(self):
         if self.pushButton_camera.isChecked():
@@ -31,10 +33,19 @@ class GUIMainWindow(Ui_MainWindow, QtWidgets.QMainWindow):
             self.pushButton_camera.setText('Camera')
             self.camera_thread.flag = False
 
+    def camera_capture(self):
+        # self.camera_thread.flag = False
+        self.camera_thread.camera.set_Capture_ready()
+        self.camera_thread.camera.get_Capture_image()
+        # self.camera_thread.flag = True
+        # self.camera_thread.start()
+
     def camera_show(self, image):
         frame = jpeg.decode(image)
         Qimg = QtGui.QImage(frame.data, frame.shape[1], frame.shape[0], QtGui.QImage.Format_RGB888)
+        Qimg = Qimg.rgbSwapped()
         pixmap = QtGui.QPixmap.fromImage(Qimg)
+
         self.label_image_show.setPixmap(pixmap)
 
 
@@ -54,7 +65,8 @@ class CameraThread(QtCore.QThread):
             self.data = self.camera.get_Live_image()
             if (self.data.size != 0) and (self.data[0] != 0):
                 self.CameraSignal.emit(self.data)
-        self.camera.Terminate()
+                time.sleep(0.1)
+        self.camera.Release_Live()
 
 
 
--- new_test.py
+++ new_test.py
@@ -1,12 +1,21 @@
 import CanonLib
+from turbojpeg import TurboJPEG
+jpeg = TurboJPEG("C:\\turbojpeg.dll")
+import cv2
 
 c = CanonLib.CanonCamera()
-err = c.Init_Camera()
-if type(err) is str:
-    print(err)
-err = c.set_LiveView_ready()
-if type(err) is str:
-    print(err)
-err = c.get_Live_image()
-if type(err) is str:
-    print(err)
+c.Init_Camera()
+c.set_LiveView_ready()
+while True:
+    data = c.get_Live_image()
+    if (data.size != 0) and (data[0] != 0):
+
+        frame = jpeg.decode(data)
+        # frame = numpy.reshape(data, (3456, 5184, 3))
+        # # frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        cv2.imshow("SimpleLive_Python_uEye_OpenCV", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+c.Terminate()
